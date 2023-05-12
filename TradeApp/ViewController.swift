998c3239912e6be86fd4ea5498c2029814d6f0db
Artem <artem-orlov92@bk.ru>
2023-05-12 16:17:17 +0300

    Initial Commit

---
+++ TradeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeApp
+//
+//  Created by Артем Орлов on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
